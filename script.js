09bf3122578d801f300fe543c8c7875c6d8e085b
CHOWDAM JAGANATH <123239098+JaganathC@users.noreply.github.com>
2024-02-14 22:41:45 +0530

    Update and rename Wikipedia.js to script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const prompt = require('prompt-sync')();
 let searchInputEl = document.getElementById("searchInput");
 let searchResultsEl = document.getElementById("searchResults");
-let spinner = document.getElementById("spinner");
 
 function create(result) {
     let {
@@ -9,22 +7,22 @@ function create(result) {
         link,
         description
     } = result;
-    //creating Result Item --Div
+ 
     let res = document.createElement("div");
     res.classList.add("result-item");
     searchResultsEl.appendChild(res);
 
-    //Creating Title Element --Anchor
+    
     let resTitleEl = document.createElement("a");
     resTitleEl.classList.add("result-item");
     resTitleEl.textContent = title;
     resTitleEl.href = link;
     resTitleEl.target = "_blank";
     res.appendChild(resTitleEl);
-    //Creating Break Element 
+    
     let titleBreak = document.createElement("br");
     res.appendChild(titleBreak);
-    //Creating URL Element --Anchor
+   
     let urlEl = document.createElement("a");
     urlEl.classList.add("result-url");
     urlEl.href = link;
@@ -32,10 +30,10 @@ function create(result) {
     urlEl.textContent = link;
     res.appendChild(urlEl);
 
-    //Creating Break Element 
+    
     let lineBr = document.createElement("br");
     res.appendChild(lineBr);
-    //Creating Description Element--PARAGRAPH
+    
     let paraEl = document.createElement("p");
     paraEl.classList.add("line-description");
     paraEl.textContent = description;
@@ -43,7 +41,6 @@ function create(result) {
 }
 
 function displayResults(searchResults) {
-    spinner.classList.toggle("d-none");
     for (let result of searchResults) {
         create(result);
     }
@@ -52,7 +49,6 @@ function displayResults(searchResults) {
 function search(event) {
     if (event.key === "Enter") {
         searchResultsEl.textContent = "";
-        spinner.classList.toggle("d-none");
         let searchInp = searchInputEl.value;
         let url = "https://apis.ccbp.in/wiki-search?search=" + searchInp;
         let options = {
